0197479bbcfb646c52413c7d91ecbe5cd49a3146
kakakukuki <125250109+kakakukuki@users.noreply.github.com>
2023-03-04 15:01:03 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Peter is AWESOME!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  <center><h2>Welcome to "Peter is AWESOME" World!</h2></center>
+  This is ${PREFIX}'s app.
   <!-- END -->
 
   </div>
